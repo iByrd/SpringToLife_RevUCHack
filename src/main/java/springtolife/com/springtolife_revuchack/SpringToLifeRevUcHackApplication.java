6b75da70e9a05f6a3bebf2110fd43ec97bf18f46
Mike Byrd <122894342+iByrd@users.noreply.github.com>
2024-02-25 00:10:55 -0500

    Initial commit

---
+++ src/main/java/springtolife/com/springtolife_revuchack/SpringToLifeRevUcHackApplication.java
@@ -0,0 +1,13 @@
+package springtolife.com.springtolife_revuchack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringToLifeRevUcHackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringToLifeRevUcHackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springtolife/com/springtolife_revuchack/SpringToLifeRevUcHackApplicationTests.java
@@ -0,0 +1,13 @@
+package springtolife.com.springtolife_revuchack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringToLifeRevUcHackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
